2681772fedb1863dbf5de7fa78daaebfd9c4dc8c
Lucenajj <lucenajj@gmail.com>
2020-03-09 23:28:14 -0300

    adicionar estilização ao body

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function iniciarGame() {
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;    
 
     for (i = 1; i < snake.length; i++) {
-        if (snake[0].x) == snake[i].x && snake[0].y == snake[i].y {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
             clearInterval(jogo);
             alert('Game Over :(')
         }        
